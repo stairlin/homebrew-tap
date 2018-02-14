a374145762c7bc54ea8f587b4495a9d5d3903429
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-14 13:34:26 +0100

    kargo version v1.0.1

--- kargo.rb
+++ kargo.rb
@@ -3,9 +3,7 @@ class Kargo < Formula
   homepage "https://github.com/stairlin/kargo"
   url "https://github.com/stairlin/kargo/releases/download/v1.0.1/kargo_1.0.1_darwin_amd64.tar.gz"
   version "1.0.1"
-  sha256 "5f4453675b44f4b2cb0ffd7744538a0ff03e96da9d38f988434c3abb4411e6d0"
-  
-  depends_on "tar"
+  sha256 "c6f4ab5f8e28b745749aed73cceabd6b5a13467e93c4eb6a9928affdaa947915"
 
   def install
     bin.install "kargo"
