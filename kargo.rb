2b186cbb2aa7321f67f8bc30310fa4ee82d9caba
stairlin <it@stairlin.com>
2018-04-20 13:50:07 +0200

    kargo version v1.0.6

--- kargo.rb
+++ kargo.rb
@@ -1,9 +1,9 @@
 class Kargo < Formula
   desc "The plugin-driven CLI for backing up & restoring data."
   homepage "https://github.com/stairlin/kargo"
-  url "https://github.com/stairlin/kargo/releases/download/v1.0.5/kargo_1.0.5_darwin_amd64.tar.gz"
-  version "1.0.5"
-  sha256 "643d70eab4d06037226efd632b10ac615f676e85307ac23cdd6a8ecb9411c55d"
+  url "https://github.com/stairlin/kargo/releases/download/v1.0.6/kargo_1.0.6_darwin_amd64.tar.gz"
+  version "1.0.6"
+  sha256 "d9cf9005e3f931649729af72e63d522425dcfb1206105c96d8bb72404b30f099"
 
   def install
     bin.install "kargo"
